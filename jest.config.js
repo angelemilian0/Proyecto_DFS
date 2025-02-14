254b0c330079e53098f4d45b489ad6a1d8e280ea
Angel Esparza <al07003423@tecmilenio.mx>
2025-02-13 21:51:40 -0600

    Actualización del proyecto y configuración de CI/CD

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const MONGODB_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/AvanceProyecto_DFS';
-export const PORT = process.env.PORT || 4003;
+export const PORT = process.env.PORT || 4000;
 export const JWT_SECRET = process.env.JWT_SECRET;
 
 // Agrega validación de variables de entorno
