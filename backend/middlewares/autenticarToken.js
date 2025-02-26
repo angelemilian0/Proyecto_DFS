d9f60145f10c71c9b329156672a6109fbd7219c1
angelemilian0 <al07003423@tecmilenio.mx>
2025-02-26 03:53:51 +0000

    s

--- backend/middlewares/autenticarToken.js
+++ backend/middlewares/autenticarToken.js
@@ -32,18 +32,3 @@ function autenticarToken(req, res, next) {
 
 // Exportamos la función para que pueda ser utilizada en otras partes de la aplicación
 module.exports = autenticarToken;
-
-Usuario.js:
-
-// Importamos el módulo mongoose para manejar la base de datos MongoDB
-const mongoose = require('mongoose');
-
-// Definimos el esquema para la colección de usuarios
-const UsuarioSchema = new mongoose.Schema({
-  nombre: { type: String, required: true }, // Nombre del usuario, campo obligatorio
-  email: { type: String, required: true, unique: true }, // Correo electrónico único y obligatorio
-  password: { type: String, required: true }, // Contraseña del usuario, campo obligatorio
-});
-
-// Exportamos el modelo 'Usuario' basado en el esquema definido
-module.exports = mongoose.model('Usuario', UsuarioSchema);
