bebfa249d7868dfafdaca5b0f050d515a0ec08e4
angelemilian0 <al07003423@tecmilenio.mx>
2025-02-27 20:56:14 +0000

    xxx

--- backend/index.js
+++ backend/index.js
@@ -26,25 +26,24 @@ const connectDB = async () => {
 
 connectDB();
 
+// 🔹 Mover esto arriba para que Express maneje los archivos estáticos primero
+app.use(express.static(path.join(__dirname, '../frontend')));
+
 // Rutas
 const usuarioRoutes = require('./routes/usuario');
 app.use('/api/usuarios', usuarioRoutes);
 const climaRoutes = require('./routes/clima');
 app.use('/api/clima', climaRoutes);
 
+// 🔹 Manejamos cualquier otra ruta no definida para que siempre sirva el index.html
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/index.html'));
+});
 
 // Iniciar el servidor solo si no es entorno de prueba
 const PORT = process.env.PORT || 4003;
 if (process.env.NODE_ENV !== 'test') {
     app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
 }
-
-// Servir archivos estáticos desde la carpeta frontend
-app.use(express.static(path.join(__dirname, '../frontend')));
-
-// Ruta por defecto para servir el archivo HTML principal
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/index.html'));
-});
 
 module.exports = app;
